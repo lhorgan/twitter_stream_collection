b8132bd4b736ba0b550b620eb7b5389255b07c3e
Luke Horgan <horgan.l@husky.neu.edu>
2020-10-14 00:54:58 -0400

    woops, this too!

--- flow.sh
+++ flow.sh
@@ -3,7 +3,7 @@
 # https://superuser.com/questions/1218575/best-way-to-auto-restart-mono-process-after-crash
 while (true) do
     echo "Here we go! You have a life vest, right?"
-    python3 stream.py
+    python3 stream.py ./data/
     exitcode=$?
     echo "Stream crashed with exit code $exitcode"
 done
